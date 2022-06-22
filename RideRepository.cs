713c99a4dabbdf9daf7aa6c72afe1dc5fa5d582d
Ambili das v s <ambili.kan@gmail.com>
2022-06-22 21:31:14 +0530

    Calculating Fare

--- RideRepository.cs
+++ RideRepository.cs
@@ -8,14 +8,6 @@ namespace CabInvoiceGenerator
 {
     public class RideRepository
     {
-        //Dictionary to store userid and rides in list
-        Dictionary<string, List<Ride>> userRides = null;
-
-        //Constructor to create dictionary
-        public RideRepository()
-        {
-            this.userRides = new Dictionary<string, List<Ride>>();
-        }
-        //Method to add ride list to specified user id
+        
     }
 }
